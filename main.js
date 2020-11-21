2daa32a65bb6f167278831fcd106f6a463c90ff7
myleo1 <504861978@qq.com>
2020-11-21 13:44:30 +0800

    update

--- main.js
+++ main.js
@@ -17,10 +17,12 @@ let svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
 
-svg.attr("class", "background")
+svg.append("rect")
+    .attr("fill", "#fff")
+    .attr("class", "background")
     .attr("width", width)
     .attr("height", height)
-//todo click
+    .on("click", clicked);
 
 let mapG = svg.append("g")
     .attr("id", "mapG")
@@ -45,10 +47,11 @@ this.geoJson().then(data => {
         .append("path")
         .attr("stroke", color.lineColor)
         .attr("stroke-width", 1)
-        .attr("opacity", 0.5)
+        // .attr("fill", "#000")
         .attr("id", function (d) {
             return "path" + d.properties.id;
         })
+        .attr("style", "display:block")
         .attr("class", function (d) {
             if (d.properties.id.length > 2) {
                 return "location"
@@ -59,39 +62,108 @@ this.geoJson().then(data => {
         .attr("style", "cursor:pointer")
         .attr("d", path)
         .on('mouseover', function (d) {
-            d3.select(this).attr("stroke", "#fbcd2c")
-                .attr("stroke-width", 3)
+            d3.select(this).attr("stroke-width", 5 / self.scale + "px");
+            d3.select(this).attr("stroke", color.pointColor);
+        })
+        .on('mousemove', function (d, i) {
+            let info = d.target.__data__.properties
+            // console.log(d)
+            let x = d.pageX;
+            let y = d.pageY;
+            d3.select("#tooltip").remove();
+            d3.select('svg').append("text")
+                .attr("id", "tooltip")
+                .attr("x", x + 18)
+                .attr("y", y + 18)
+                .attr("text-anchor", "middle")
+                .attr("font-family", "sans-setif")
+                .attr("font-size", "11px")
+                .attr("font-weight", "bold")
+                .attr("fill", "#D36611")
+                .text("当前位置" + ":" + info.name)
         })
         .on('mouseout', function () {
-            d3.select(this).attr("stroke", color.lineColor)
-                .attr("stroke-width", 1)
+            d3.select(this)
+                .attr("stroke", color.lineColor)
+                .attr("stroke-width", 1 / scale + "px");
+            d3.select("#tooltip").remove();
         })
         .on("click", function (d) {
             if (d.target.__data__.properties.id.length <= 2) {
-                clicked(d.target.__data__)
+                clicked(d.target.__data__, 1)
+            }
+            if (d.target.__data__.properties.id.length > 2) {
+                let info = d.target.__data__.properties
+                getWeather(info.id, 2).then(weatherData => {
+                    let x = d.pageX;
+                    let y = d.pageY;
+                    d3.select("#tooltip").remove();
+                    let s = "实时天气" + "，"
+                        + " ，"
+                        + "当前城市：" + weatherData.live[0].city + "，"
+                        + "天气：" + weatherData.live[0].weather + "，"
+                        + "温度：" + weatherData.live[0].temperature + "℃" + "，"
+                        + "湿度：" + weatherData.live[0].humidity + "，"
+                        + "风向：" + weatherData.live[0].winddirection + "，"
+                        + "风力：" + weatherData.live[0].windpower + "级" + "，"
+                        + "更新时间：" + weatherData.live[0].reporttime + "，"
+                    let text = d3.select("svg").append("text")
+                        .attr("id", "tooltip")
+                        .attr("x", x + 18)
+                        .attr("y", y + 18)
+                        .attr("text-anchor", "middle")
+                        .attr("font-family", "sans-setif")
+                        .attr("font-size", "15px")
+                        .attr("font-weight", "bold")
+                        .attr("fill", "#FF8C00")
+                    let str = s.split("，");
+                    text.selectAll("tspan")
+                        .data(str)
+                        .enter()
+                        .append("tspan")
+                        .attr("x", text.attr("x"))
+                        .attr("dy", "1em")
+                        .text(function (d) {
+                            return d;
+                        });
+                })
             }
         })
+
 });
 
-function clicked(d) {
+function clicked(d, flag) {
     let x, y, k;
-    if (d && centered !== d) {
-        //对指定的功能feature计算投影重心
-        let centroid = path.centroid(d)
-        x = centroid[0]
-        y = centroid[1]
-        if (d.properties.id === "46") {
-            y = y + 10;
+    if (flag === 1) {
+        if (centered !== d) {
+            //对指定的功能feature计算投影重心
+            let centroid = path.centroid(d)
+            x = centroid[0]
+            y = centroid[1]
+            if (d.properties.id === "46") {
+                y = y + 10;
+            }
+            if (d.properties.scale) {
+                k = d.properties.scale
+            } else {
+                k = scale
+            }
+            scale = k
+            centered = d
+            mapChange("open", d)
         }
-        if (d.properties.scale) {
-            k = d.properties.scale
-        } else {
-            k = scale
-        }
-        scale = k
-        centered = d
-        mapChange("open", d)
+    } else {
+        x = width / 2;
+        y = height / 2;
+        k = 1;
+        scale = 1;
+        centered = null;
+        mapChange("close");
     }
+    mapG.selectAll("path")
+        .classed("active", centered && function (d) {
+            return d === centered;
+        });
     mapG.transition()
         .duration(750)
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
@@ -104,30 +176,53 @@ function mapChange(flag, d) {
         if (disId.length < 3) {
             this.geoJson().then(data => {
                 data.features.forEach(function (n) {
-                    //遍历所有id，如果id>2(不是省) & 前2位和传入的id相等(属于该省)
+                    //如果是属于该省的地级市
                     if (n.properties.id.length > 2 && n.properties.id.substr(0, 2) === disId) {
                         //必须加#才起效果
                         d3.select("#path" + n.properties.id)
+                            .attr("style", "display:block")
                             .attr("stroke", color.lineColor)
                             .attr("stroke-width", 1 / scale + "px")
                             .attr("fill", "#1b5c5b")
                     } else {
-                        //todo
+                        //如果是省 & 并且是点击之外的其他省
+                        if (n.properties.id.length <= 2 && n.properties.id !== disId) {
+                            d3.select(".point-info").attr("style", "display:none;");
+                            d3.select("#path" + n.properties.id)
+                                //隐藏其他省份
+                                .attr("style", "display:none")
+                                .attr("stroke-width", "0.3px");
+                            // d3.select("#text" + n.properties.id)
+                            //     .attr("style", "display:none")
+                            //     .attr("font-size", "3px");
+                        } else {
+                            //如果是其他地级市
+                            d3.select("#path" + n.properties.id)
+                                .attr("style", "display:none")
+                                .attr("stroke-width", "0.2px");
+                            // d3.select("#text" + n.properties.id)
+                            //     .attr("style", "display:none")
+                            //     .attr("font-size", "1px");
+                        }
                     }
                 })
-
-
             })
         }
+    } else {
+        d3.selectAll(".location").attr("style", "display:none");
+        // d3.selectAll("text").attr("font-size", "14px");
+        d3.selectAll("path").attr("stroke-width", "1px");
+        // d3.selectAll(".distribution").attr("style", "display:block");
+        d3.selectAll(".distribution").attr("style", "cursor:pointer");
+
     }
 }
 
 
-
 //天气api部分
-getWeather(3301, 1)
+// getWeather(3301, 1)
 
-function getWeather(location, flag) {
+async function getWeather(location, flag) {
     let formData = new FormData();
     if (flag === 1) {
         formData.append("location", location)
@@ -145,7 +240,9 @@ function getWeather(location, flag) {
         },
         body: formData
     };
-    fetch('http://111.0.80.7:7654/weather-server-go/rest/weather/getWeather', options).then(res => res.json())
-        .then(weatherData => console.log(weatherData))
-        .catch(err => console.log('err:', err))
+    const weather = await fetch('http://111.0.80.7:7654/weather-server-go/rest/weather/getWeather', options).then(res => res.json())
+    // .then(weatherData => console.log(weatherData))
+    // .catch(err => console.log('err:', err))
+    console.log(weather.data)
+    return weather.data
 }
